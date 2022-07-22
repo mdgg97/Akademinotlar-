0d45f099519894c20de00a89177267a8b54d66f7
Mustafa Deniz Gökcan <60901873+mdgg97@users.noreply.github.com>
2022-07-22 11:24:07 +0300

    Create bolumodevi1.py

---
+++ bolumodevi1.py
@@ -0,0 +1,9 @@
+ahmet = 22
+mehmet = 22
+
+if ahmet > mehmet:
+    print("Ahmet Mehmetten büyüktür.")
+elif mehmet > ahmet:
+    print("Mehmet Ahmetten büyüktür.")
+else:
+    print("Mehmet ve Ahmet aynı yaştadır.")
