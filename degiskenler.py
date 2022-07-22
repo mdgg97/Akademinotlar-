dbe6239035dff6760aa8b78b6e82029671aa457b
Mustafa Deniz Gökcan <60901873+mdgg97@users.noreply.github.com>
2022-07-22 11:22:20 +0300

    Create degiskenler.py

---
+++ degiskenler.py
@@ -0,0 +1,21 @@
+# baslik = "HABERİNİZ OLSUN" #string
+# vade = 12 #integer
+# Faiz0rani1 = 1.47 #float
+# Faiz0rani2 = 1.44 #float
+# Faiz0rani1 = 1.47
+
+# print(baslik)
+# print(type(baslik))
+# print(type(vade))
+# print(type(Faiz0rani1))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Mustafa Deniz"
+musteriSoyadi = "Gökcan"
+sonucMesaj = mesaj+" "+musteriAdi+" "+musteriSoyadi
+print(sonucMesaj)
+
+sayi1 = 25
+sayi2 = 30
+print(sayi1 + sayi2)
+print(sonucMesaj)
