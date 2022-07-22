9f745d7f1d332e321c35c42bfc8eabffe1442eca
Mustafa Deniz Gökcan <60901873+mdgg97@users.noreply.github.com>
2022-07-22 11:23:35 +0300

    Create else.py

---
+++ else.py
@@ -0,0 +1,17 @@
+# Sart 
+dolarDun = 7.75
+dolarBugun = 7.75
+
+if dolarDun > dolarBugun:
+  print("Azalış oku")
+  print("Bitti")
+elif dolarDun < dolarBugun:
+   print("Artış oku")
+  
+else:
+  print("Eşittir oku")
+# if dolarDun < dolarBugun:
+#   print("Artış oku")
+
+# if dolarDun == dolarBugun:
+#   print("Eşittir oku")
