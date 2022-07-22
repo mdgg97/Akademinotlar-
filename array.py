6031c500bea8cac897f94ee06f55c0b7ecfe79ed
Mustafa Deniz Gökcan <60901873+mdgg97@users.noreply.github.com>
2022-07-22 11:27:06 +0300

    Create array.py

---
+++ array.py
@@ -0,0 +1,13 @@
+krediler = [
+    "Hızlı Kredi ", "Maaşını Halkbank'tan alanlara özel",
+    "Mutlu Emekli İhtiyaç Kredisi"
+]
+
+print(krediler)
+print(krediler[0])
+print(krediler[1])
+print(krediler[2])
+print(len(krediler))  #length
+
+krediler[0] = "Çabuk Kredi"
+print(krediler)
